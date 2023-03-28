214942e1bbcf12755f1e40c18c3b0894e69ad605
Daniel <danieruone@gmail.com>
2023-03-28 15:25:18 -0500

    Clean project and add icons

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -6,10 +6,30 @@ interface DefaultTheme {
 }
 
 export const GlobalStyles = createGlobalStyle<{ theme: DefaultTheme }>`
+  :root {
+    font-family: Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
+    line-height: 1.5;
+    font-weight: 400;
+
+    color-scheme: light dark;
+    color: rgba(255, 255, 255, 0.87);
+    background-color: #242424;
+
+    font-synthesis: none;
+    text-rendering: optimizeLegibility;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-text-size-adjust: 100%;
+  }
+  
   body {
     background: ${({ theme }) => theme.body};
     color: ${({ theme }) => theme.text};
     transition: background 0.2s ease-in, color 0.2s ease-in;
+    margin: 0;
+    min-width: 320px;
+    min-height: 100vh;
+    width: 100%;
   }
 `;
 
