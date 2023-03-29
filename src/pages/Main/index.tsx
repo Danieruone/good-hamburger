707a9c5d2ee084da097c892bc1c3fdc3c2effdf8
Daniel <danieruone@gmail.com>
2023-03-28 21:44:37 -0500

    Add food filter logic

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,14 +1,27 @@
 // styles
+import { useEffect, useState } from 'react';
 import { Container, Header } from './styles';
 
 // components
 import { ThemeToggler } from 'components/ThemeToggler';
 import { FoodCard } from 'components/FoodCard';
+import { FoodFilter } from 'components/FoodFilter';
 
 // menu
 import { menuArray } from 'menu';
 
 export const Main = () => {
+  const [currentFoodType, setCurrentFoodType] = useState('all');
+  const [filteredFood, setFilteredFood] = useState(menuArray);
+
+  useEffect(() => {
+    setFilteredFood(
+      currentFoodType === 'all'
+        ? menuArray
+        : menuArray.filter((foodObject) => foodObject.type === currentFoodType)
+    );
+  }, [currentFoodType]);
+
   return (
     <Container>
       <Header>
@@ -16,7 +29,12 @@ export const Main = () => {
         <ThemeToggler />
       </Header>
 
-      {menuArray.map((foodItem, idx) => (
+      <FoodFilter
+        currentFoodType={currentFoodType}
+        setCurrentFoodType={setCurrentFoodType}
+      />
+
+      {filteredFood.map((foodItem, idx) => (
         <FoodCard {...foodItem} key={idx} />
       ))}
     </Container>
